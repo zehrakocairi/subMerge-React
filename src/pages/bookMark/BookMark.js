649d1e62b88c6c7a993603e5e613a1ebf5706f60
zehra <zehrakocairi@gmail.com>
2024-01-15 15:03:34 +0100

    small change

--- src/pages/bookMark/BookMark.js
+++ src/pages/bookMark/BookMark.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import MovieSubtitleCard from "../../components/MovieSubtitleCard/MovieSubtitleCard";
 
-function BookMark() {
+function Bookmark() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -22,4 +22,4 @@ function BookMark() {
   );
 }
 
-export default BookMark;
+export default Bookmark;
